8a405632c7ee980cbe15bd1168a1e646a8c69855
Andrei Salomatov <asalomatov@bdtherapeutics.com>
2022-04-21 10:07:34 -0400

    added binary classification metrics

--- python/ml_utils.py
+++ python/ml_utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn import model_selection
+from sklearn import metrics
 
 def split_data(df, targ_clm, train_perc=0.7, rand_state=212):
     """Train/Validation split data frame.
@@ -72,6 +73,42 @@ def create_folds_regr(df, targ_clm, k_folds=5):
     df = df.drop("bins", axis=1)
     return(df)
 
+def binary_classification_metrics(y_true, y_pred, y_prob=None):
+    """ y_true - true labels
+        y_pred - predicted labels
+        y_prob - predicted probs"""
+    confusion = metrics.confusion_matrix(y_true, y_pred)
+    TP = confusion[1, 1]
+    TN = confusion[0, 0]
+    FP = confusion[0, 1]
+    FN = confusion[1, 0]
+    P = TP + FN
+    N = TN + FP
+    res = pd.DataFrame({
+        'Prevalence': [P/(P + N)],
+        'Sensitivity': [TP/P], # recall, TPR
+        'Sensitivity_skl': [metrics.recall_score(y_true, y_pred)],
+        'Specificity': [TN/N],
+        'Accuracy': [(TP + TN)/(P + N)],
+        'Accuracy_skl': [metrics.accuracy_score(y_true, y_pred)],
+        'Precision': [TP/(TP + FP)], # also known as PPV
+        'Precision_skl': [metrics.precision_score(y_true, y_pred)],
+        'F1': [2*TP/(2*TP + FP + FN)],
+        'F1_skl': [metrics.f1_score],
+        'FPR': [FP/N], # 1 - specificity
+        'MCC': [(TP * TN - FP * FN)/((TP + FP) * (FN + TN) * (FP + TN) * (TP + FN))**0.5],
+        'MCC_skl': [metrics.metrics.matthews_corrcoef]
+    })
+    if y_prob is not None:
+        res['AUC'] = metrics.roc_auc_score(y_true, y_prob)
+        res['LogLoss'] = metrics.log_loss(y_true, y_prob)
+    return(res)
+
+def feature_clms(df_clms, targ_clm):
+    """return list of feature columns"""
+    res = [i for i in df_clms if i != targ_clm]
+    return(res)
+
 
 if __name__ == "__main__":
     from sklearn.datasets import make_classification
@@ -86,6 +123,15 @@ if __name__ == "__main__":
     df = diab.frame
     df.loc[:, 'class'] = df.loc[:, 'class'].map({'tested_positive': 1,
                                                 'tested_negative': 0})
-    l = split_data(df, 'class')
+    df_trn, dr_val = split_data(df, 'class')
+    from sklearn.linear_model import LogisticRegression
+
+    clf = LogisticRegression(random_state=212).fit(
+        df_trn[feature_clms(df_trn.columns, 'class')],
+        df_trn['class'])
+    y_pred = clf.predict(df_trn[feature_clms(df_trn.columns, 'class')])
+    y_prob = clf.predict_proba(df_trn[feature_clms(df_trn.columns, 'class')])
+    print(binary_classification_metrics(df_val['class'], y_pred))
+    print(binary_classification_metrics(df_val['class'], y_pred, y_prob=y_prob))
 
     df_trn = create_folds(l[0], 'class')
