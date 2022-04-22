0552ee835be8f22bbd5643cb51b05476a073a650
Andrei Salomatov <asalomatov@bdtherapeutics.com>
2022-04-22 15:44:11 -0400

    config variables

---
+++ python/config.py
@@ -0,0 +1,12 @@
+
+MODEL_OUTPUT = "../models/"
+
+# PREFIX = "mnist"
+# TRAINING_FILE = "../input/mnist_train_folds.csv" 
+# VALIDATION_FILE = "../input/mnist_validate.csv" 
+
+PREFIX = "diabetes"
+INPUT_DATA = "../input/diabetes.csv"
+TARG_CLM = "Outcome"
+TRAINING_FILE = "../input/diabetes_train_folds.csv" 
+VALIDATION_FILE = "../input/diabetes_validate.csv" 
