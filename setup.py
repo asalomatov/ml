d160e1100ef4d0c2f302662eda9c9a644fcfb1ce
Andrei Salomatov <andrei.salomatov@gmail.com>
2023-02-21 18:16:11 -0500

    renamed

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
+packages_found = find_packages()
+print('Found packages:')
+print(packages_found)
+
 setup(
-    name='ML',
+    name='usefulml',
     version='1.0',
     url='https://github.com/asalomatov/ML',
     author='Andrei Salomatov',
