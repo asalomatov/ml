2469040768b21eb971963750225ed4f206ae15f9
Andrei Salomatov <andrei.salomatov@gmail.com>
2023-02-21 17:23:47 -0500

    added setup and init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ML',
+    version='1.0',
+    url='https://github.com/asalomatov/ML',
+    author='Andrei Salomatov',
+    author_email='andrei.salomatov@gmail.com',
+    py_modules=find_packages(),
+)
+
