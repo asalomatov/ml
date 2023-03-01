17c0e5b6db07294ad94e59604f214dfa0da95eb7
Andrei Salomatov <andrei.salomatov@gmail.com>
2023-02-28 21:40:08 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
 from setuptools import setup, find_packages
 
-packages_found = find_packages()
-print('Found packages:')
-print(packages_found)
-
 setup(
     name='usefulml',
     version='1.0',
     url='https://github.com/asalomatov/ML',
     author='Andrei Salomatov',
     author_email='andrei.salomatov@gmail.com',
-    py_modules=find_packages(),
+    packages=find_packages()
 )
 
